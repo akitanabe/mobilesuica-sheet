34b573a9c72711c9d9bbdcf5275c1d092714b91d
akitanabe <tanabe@determaind.biz>
2023-02-07 07:37:18 +0000

    vue,tsファイルの整形をeslintでやるようにした。

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,15 @@
 module.exports = {
   root: true,
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
+  extends: ['@nuxtjs/eslint-config-typescript'],
   rules: {
+    // 末尾セミコロンは必須
     semi: ['error', 'always'],
     'semi-spacing': ['error', { after: true, before: false }],
     'semi-style': ['error', 'last'],
     'no-extra-semi': 'error',
     'no-unexpected-multiline': 'error',
     'no-unreachable': 'error',
+    // ケツカンマは必須
+    'comma-dangle': ['error', 'always-multiline'],
   },
 };
