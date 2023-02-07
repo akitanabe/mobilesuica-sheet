6df6697530a2017c76d7eea84311e8d92a122c79
akitanabe <tanabe@determaind.biz>
2023-02-07 11:54:38 +0000

    同じにするには設定項目が多いのでprettierを入れ直した

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,4 @@
 module.exports = {
   root: true,
-  extends: ['@nuxtjs/eslint-config-typescript'],
-  rules: {
-    // 末尾セミコロンは必須
-    semi: ['error', 'always'],
-    'semi-spacing': ['error', { after: true, before: false }],
-    'semi-style': ['error', 'last'],
-    'no-extra-semi': 'error',
-    'no-unexpected-multiline': 'error',
-    'no-unreachable': 'error',
-    // ケツカンマは必須
-    'comma-dangle': ['error', 'always-multiline'],
-  },
+  extends: ['@nuxtjs/eslint-config-typescript', 'prettier'],
 };
