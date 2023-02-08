b0f47aa535acc7d426bc247713e46f43a5b09195
akitanabe <tanabe@determaind.biz>
2023-02-08 07:38:41 +0000

    ログインフォームを追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,14 @@
 <template>
   <v-app>
     <the-header></the-header>
-    <v-main>
-      <v-container> <slot /></v-container>
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="3" />
+          <v-col cols="12" sm="6"><slot /></v-col>
+          <v-col cols="12" sm="3" />
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { Ref } from 'vue';
+
+  const loginType: Ref<'MobileSuica' | 'MyJR-EAST'> = ref('MobileSuica');
+  const password = ref('');
+  const email = ref('');
+</script>
 
 <template>
-  <div>testtest</div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="2" />
+      <v-col cols="12" sm="8">
+        <h2>モバイルSuica ログイン</h2>
+        <v-form>
+          <v-radio-group v-model="loginType" inline>
+            <v-radio label="モバイルSuica" value="MobileSuica"></v-radio>
+            <v-radio label="My JR-EAST" value="MyJR-EAST"></v-radio>
+          </v-radio-group>
+          <v-text-field v-model="email" required label="メールアドレス" />
+          <v-text-field v-model="password" required label="パスワード" />
+        </v-form>
+      </v-col>
+      <v-col cols="12" sm="2" />
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped></style>
